a81125ec456498705fca474ab2c721d55438ceec
허창우 <heochang-u@heochang-us-MacBook-Air.local>
2026-05-05 16:18:00 +0900

    Debugging Issue(data connection MongoDB)

--- app.js
+++ app.js
@@ -43,11 +43,12 @@ if(process.env.NODE_ENV !== 'production') {
     const server = app.listen(PORT, async () => {
         console
         .log(`Subcription Tracker API is running on http://localhost:${PORT}`);
-    
+
         await connectToDatabase();
     });
 } else {
     try {
+        console.log("DB URI Exists: ", !!process.env.DB_URI);
         await connectToDatabase();
     } catch(error) {
         throw error;
